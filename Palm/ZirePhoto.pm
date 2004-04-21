8561f5c02de62881831141ef934cbf0563c35ac4
christophe <christophe@81ec83ca-9948-11dd-8e58-870c635edf7a>
2004-04-21 02:14:53 +0000

    1. fixed problem with handling deleted records. When a photo database record is deleted, the thumbnail entry gets truncated such that the data section is zero bytes. I'm assuming that this is just a space saving measure since the record id is adequate for purging the record during sync.
    2. added POD documentation.

--- Palm/ZirePhoto.pm
+++ Palm/ZirePhoto.pm
@@ -1,4 +1,4 @@
-# Palm::TealMeal.pm
+# Palm::ZirePhoto.pm
 # 
 # Perl class for dealing with Zire71 photo database.
 #
@@ -6,7 +6,7 @@
 #	You may distribute this file under the terms of the Artistic
 #	License, as specified in the README file.
 #
-# $Id: ZirePhoto.pm,v 1.2 2003-09-16 22:53:47 azummo Exp $
+# $Id: ZirePhoto.pm,v 1.3 2004-04-21 02:14:53 christophe Exp $
 
 
 use strict;
@@ -16,9 +16,75 @@ use Palm::Raw();
 use vars qw( $VERSION @ISA );
 
 # One liner, to allow MakeMaker to work.
-$VERSION = do { my @r = (q$Revision: 1.2 $ =~ /\d+/g); sprintf "%d."."%02d" x $#r, @r };
+$VERSION = do { my @r = (q$Revision: 1.3 $ =~ /\d+/g); sprintf "%d."."%02d" x $#r, @r };
 
 @ISA = qw( Palm::Raw );
+
+=head1 NAME
+
+Palm::ZirePhoto - Handler for Palm Zire71 Photo thumbnail databases.
+
+=head1 SYNOPSIS
+
+    use Palm::ZirePhoto;
+
+=head1 DESCRIPTION
+
+The Zire71 PDB handler is a helper class for the L<Palm::PDB> package. It parses Zire71
+Photo thumbnail databases (and, hopefully, Tungsten Photo databases). Actual photos
+are separate databases and must be processed separately.
+
+This database is currently only capable of reading.
+
+=head2 AppInfo block
+
+The AppInfo block begins with standard category support. See
+L<Palm::StdAppInfo> for details.
+
+=head2 Records
+
+    $record = $pdb->{records}[N]
+
+    $record->{'width'}
+    $record->{'height'}
+    $record->{'size'}
+
+The actual JPEG images dimensions and (compressed) file size.
+
+    $record->{'thumb'}
+
+The thumbnail is a very small (max size approx 84x84) JPEG format image.
+
+    $record->{'name'}
+
+Image name. Appending C<.jpg> to this will give the database name of the actual image
+data.
+
+    $record->{'time1'}
+    $record->{'time2'}
+
+Unix epoch time of when the image was last modified (C<time1>) and when it was
+created (C<time2>).
+
+=head2 Photo Databases
+
+Actual photos are stored in separate databases. Each record is preceeded by an 8 byte
+header that describes it a) as a data block (B<DBLK>) and b) the size of the block.
+Records are generally 4k, except for the last. To convert a Photo database to a JPEG
+image, one would do something like:
+
+	use Palm::Raw;
+
+	my $pdb = new Palm::PDB;
+	$pdb->Load( "image.jpg.pdb" );
+	open F, ">image.jpg";
+	for( @{$pdb->{records}} ) {
+		print F substr($_->{'data'}, 8);
+	}
+	close F;
+
+=cut
+#'
 
 sub import
 {
@@ -56,6 +122,12 @@ sub ParseRecord
 	delete $record{offset};		# This is useless
 	delete $record{data};		# No longer necessary
 
+	# when Photo thumbnail records are deleted/archived/whatever, the data section is
+	# actually set to zero length. Presumably this is so that thumbnails take up
+	# minimum space until a sync purges the records.
+
+	return \%record unless length $data > 36;
+
 	@record{
 		'width',
 		'height',
@@ -78,3 +150,17 @@ sub ParseRecord
 }
 
 1;
+
+__END__
+
+=head1 AUTHOR
+
+Alessandro Zummo E<lt>a.zummo@towertech.itE<gt>
+
+=head1 SEE ALSO
+
+Palm::PDB(3)
+
+Palm::StdAppInfo(3)
+
+=cut
