2c390af40cb3f8d38e9e677f868c92c97dd9b779
christophe <christophe@81ec83ca-9948-11dd-8e58-870c635edf7a>
2005-07-29 23:04:00 +0000

    *: add a helper method, ParseNote(), to handle the note extraction stuff.
    *: add a helper method, ParseAlbum(), to parse the Album.db files that the photo
    app uses to store meta-data on cards. This parses a memory buffer containing the
    contents of an Album.db file and returns an array of records (without
    thumbnail info) which are otherwise the same as what ParseRecord() provides.

--- Palm/ZirePhoto.pm
+++ Palm/ZirePhoto.pm
@@ -6,7 +6,7 @@
 #	You may distribute this file under the terms of the Artistic
 #	License, as specified in the README file.
 #
-# $Id: ZirePhoto.pm,v 1.7 2005-07-29 20:54:14 christophe Exp $
+# $Id: ZirePhoto.pm,v 1.8 2005-07-29 23:04:00 christophe Exp $
 
 
 use strict;
@@ -17,7 +17,7 @@ use Palm::StdAppInfo();
 use vars qw( $VERSION @ISA );
 
 # One liner, to allow MakeMaker to work.
-$VERSION = do { my @r = (q$Revision: 1.7 $ =~ /\d+/g); sprintf "%d."."%02d" x $#r, @r };
+$VERSION = do { my @r = (q$Revision: 1.8 $ =~ /\d+/g); sprintf "%d."."%02d" x $#r, @r };
 
 @ISA = qw( Palm::StdAppInfo Palm::Raw );
 
@@ -89,10 +89,7 @@ image, one would do something like:
 	}
 	close F;
 
-Notes are stored at the end of the JPEG image. The following code can be used to
-extract a note from a JPEG buffer C<$data>:
-
-	my $note = $1 if $data =~ /NOTE.{8}([^\0]+)\0*ARCPHOTOBASE.{8}$/so;
+Notes are stored at the end of the JPEG image. Use C<ParseNote> to get it.
 
 =cut
 #'
@@ -158,6 +155,75 @@ sub ParseRecord
 	$record{'name'} = substr($record{'name'}, 0, $record{'nameSize'});
 
 	return \%record;
+}
+
+=head1 METHODS
+
+Handling Palm photos can be a bit complicated. Some helper methods are exported to
+make some special cases a bit easier.
+
+=head2 ParseNote
+
+	my $photo = read_jpeg_file( "image.jpg" );
+	my $note = Palm::ZirePhoto::ParseNote($photo);
+	print "Note: $note" if defined $note;
+
+The Palm photo application stores user notes at the end of the JPEG file itself. This
+method will extract that note and return it. C<undef> is returned if the note is
+unavailable.
+
+=cut
+
+sub ParseNote
+{
+	return ($_[0] =~ /NOTE.{8}([^\0]+)\0*ARCPHOTOBASE.{8}$/so) ? $1 : undef;
+}
+
+=head2 ParseAlbum
+
+	my $album = slurp("/DCIM/Unfiled/Album.db");
+	my @records = Palm::ZirePhoto::ParseAlbum( $album );
+	print $_->{name},"\n" for( @records );
+
+Photos on memory cards are stored in subdirectories of C</DCIM>. The meta-data for
+these images are stored in C<Album.db> files under each category directory. This
+method will parse out the meta-data into an array of records similar to those
+returned by C<ParseRecord>. Thumbnail information, however, is not available.
+
+=cut
+
+sub ParseAlbum
+{
+	my $album = shift;
+
+	# make sure it's an expected record format.
+	return undef unless $album =~ /^DBFH/o;
+
+	my @records;
+
+	# skip .db file's initial 16 byte header, then grab 292 byte records
+	for( my $pos = 16; $pos < length($album); $pos += 292 ) {
+		my $buf = substr( $album, $pos, 292 );
+		last if length($buf) < 292;
+
+		my %record;
+		@record{
+			'name',
+			'time1_secs',
+			'time2_secs',
+			'size',
+			'width',
+			'height'
+		} = unpack('a256 x4 N N N x8 n n x8', $buf);
+
+		$record{name} =~ s/\0+$//o;
+		$record{time1} = $record{time1_secs} - 2082844800;
+		$record{time2} = $record{time2_secs} - 2082844800;
+
+		push @records, \%record;
+	}
+
+	return @records;
 }
 
 1;
